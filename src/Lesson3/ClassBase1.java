42c02dd976f4367bbdffe4eff60a3aac9836ca70
JihoYoon <adonis167@icloud.com>
2017-09-22 13:26:13 +0900

    base1

--- src/Lesson3/ClassBase1.java
+++ src/Lesson3/ClassBase1.java
@@ -4,9 +4,12 @@ public class ClassBase1 {
     static public void main(String args[]) {
         Constructor c = new Constructor();
         System.out.println(c.getName());
-        Constructor c1 = new Constructor("a");
+        System.out.println(c.getFriend(0));
+        Constructor c1 = new Constructor("Jiho");
         System.out.println(c1.getName());
-        Constructor c2 = new Constructor("a","b");
+        System.out.println(c1.getFriend(0));
+        Constructor c2 = new Constructor("Jiho","Yoon");
         System.out.println(c2.getName());
+        System.out.println(c2.getFriend(0));
     }
 }
--- src/Lesson3/Constructor.java
+++ src/Lesson3/Constructor.java
@@ -1,8 +1,20 @@
 package Lesson3;
 
+import java.util.ArrayList;
+
 public class Constructor {
     private String firstName;
-    private  String lastName;
+    private String lastName;
+    private String seperator;
+    private ArrayList<String> friends;
+
+    //초기화 블록
+    {
+        seperator = " ";
+        friends = new ArrayList<>();
+        friends.add("mike");
+        friends.add("jane");
+    }
 
     public Constructor() {
         this("john","doe"); //생성자에서 다른 생성자를 부를 때에만 this()가 있다.
@@ -16,5 +28,8 @@ public class Constructor {
     }
     public String getName() {
         return firstName + " " + lastName;
+    }
+    public String getFriend(int index) {
+        return friends.get(index);
     }
 }
